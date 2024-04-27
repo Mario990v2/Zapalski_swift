b6647964dbd6cb01f3f62c5509d0c8e49f662d34
user <user@account.com>
2024-04-27 14:21:41 +0200

    Initial Commit

---
+++ Zapalski_swift/Zapalski_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zapalski_swiftApp.swift
+//  Zapalski_swift
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Zapalski_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
