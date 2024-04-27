041839d1c4106b8f1fedfcd71a16da96ad520d3d
Mariusz Zapalski <pizza990@op.pl>
2024-04-27 16:31:58 +0200

    laby 22

--- Zapalski_swift/Card.swift
+++ Zapalski_swift/Card.swift
@@ -4,5 +4,39 @@
 //
 //  Created by student on 27/04/2024.
 //
-
-import Foundation
+//
+import SwiftUI
+struct Card: View {
+    @State var czyZakryta:Bool = true;
+    var buzka:String = "";
+    init(buzka: String) {
+        self.buzka = buzka
+    }
+    var body: some View {
+        let baza = RoundedRectangle(cornerRadius: 12)
+        
+    
+            Group{
+                ZStack{
+                    baza.fill(.white)
+                    baza.frame(height: 80)
+                    baza.overlay(RoundedRectangle(cornerRadius: 12)
+                        .stroke(.green,lineWidth: 2)
+                    )
+                    .onTapGesture {
+                        czyZakryta = !czyZakryta
+                    }
+                    Text(buzka).font(.largeTitle)
+                    
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(.green)
+                        .frame(height: 80)
+                        .opacity(czyZakryta ? 1 : 0)
+                        .onTapGesture {
+                            czyZakryta = !czyZakryta
+                        }
+                    
+                }
+        }.opacity(1)
+    }
+}
--- Zapalski_swift/ContentView.swift
+++ Zapalski_swift/ContentView.swift
@@ -7,17 +7,56 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @State var icony: Array<String> = ["🤠","🤯","💀","😺","😂" ,"👻","👽","😍","😎","🥳"]
+    @State var liczbaikonek = 2
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("mario99")
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
+                    ForEach(icony.prefix(liczbaikonek), id: \.self){
+                        icona in Card( buzka:icona)
+                    }
+                }
+            }
+            
+            .padding()
         }
+       
+        
+        HStack{
+            Button(action: {
+                adjustCardNumber(by: -2)
+            }) {
+                Image(systemName: "minus.circle")
+            }
+            .disabled(liczbaikonek <= 2)
+            
+            Spacer()
+            
+            Button(action: {
+                adjustCardNumber(by: 2)
+            }) {
+                Image(systemName: "plus.circle")
+            }
+            .disabled(liczbaikonek >= icony.count)
+            
+        }
+        .font(.title)
         .padding()
     }
+    func adjustCardNumber(by offset: Int) {
+        let nowaliczbaikonek = liczbaikonek + offset
+        if nowaliczbaikonek >= 2 && nowaliczbaikonek <= icony.count {
+            liczbaikonek = nowaliczbaikonek
+        }
+    }
+
 }
+
 
 #Preview {
     ContentView()
